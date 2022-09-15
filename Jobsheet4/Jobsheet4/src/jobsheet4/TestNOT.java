128afc48dd916f88ef70768a2312707d63010348
21343002 <112646969+21343002@users.noreply.github.com>
2022-09-15 21:23:27 +0700

    Add files via upload

---
+++ Jobsheet4/Jobsheet4/src/jobsheet4/TestNOT.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jobsheet4;
+
+/**
+ *
+ * created by 21343002_Aini Rizkina
+ */
+public class TestNOT {
+    public static void main(String[] argh){
+        boolean val1 = true;
+        boolean val2 = false;
+        
+        System.out.println(!val1);
+        System.out.println(!val2);
+    }
+}
