c51047bb2c1355f85ff2055dcea2eb1ad3b2a5cd
21343002 <112646969+21343002@users.noreply.github.com>
2022-09-15 21:30:12 +0700

    Add files via upload

---
+++ JS-04/src/js/pkg04/tugas1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package js.pkg04;
+
+/**
+ *
+ * created by 21343002_Aini Rizkina
+ */
+public class tugas1 {
+   public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 45;
+        
+        System.out.println("Number 1 = " + a);
+        System.out.println("Number 2 = " + b);
+        System.out.println("Number 3 = " + c);
+        System.out.println("Average = "+ (a + b + c)/3);
+    } 
+}
